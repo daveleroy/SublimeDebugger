889a33415cf7cd2e7267996f8d0d2c52ee3cd95c
David <2889367+daveleroy@users.noreply.github.com>
2018-11-01 18:30:34 -0700

    Fixes threading issue in sublime_open_file_async that sometimes causing it to never complete when a view was opened for the first time. This fixes a bug where the breakpoints hover menu was not always displayed when tapping a breakpoint in the breakpoint panel and also the paused menu to sometimes not appear at the location the program was paused.

--- core/sublime.py
+++ core/sublime.py
@@ -14,25 +14,36 @@ import sublime_plugin
 from sublime_db.libs import asyncio
 
 from .core import main_loop, async, awaitable
+from .event import Handle
 
 @async
-def sublime_open_file_async(window: sublime.Window, file: str, line: int) -> awaitable[sublime.View]:
+def sublime_open_file_async(window: sublime.Window, file: str, line: Optional[int] = None) -> awaitable[sublime.View]:
 	from sublime_db import ui
-	view = window.open_file(file)	
-	if view.is_loading():
-		future = main_loop.create_future()
+	future_view = main_loop.create_future()
 
-		# FIXME this is terrible
-		handle = ui.Handle(ui.view_loaded, None) #type: ignore
-		def loaded_view(v: sublime.View) -> None:
-			if view == v:
-				future.set_result(view)
-				handle.dispose()
-		handle.callback = loaded_view
-		ui.view_loaded.add_handle(handle)
-		yield from future
+	# Do this stuff on the main thread. Otherwise we end up with the issue that the view_loaded event could happen before we register the callback for it.
+	# and we don't want to register for the callback beforehand because we may not need to...
+	def on_sublime_main() -> None:
+		view = window.open_file(file)
+		if view.is_loading():
+			# FIXME this is terrible
+			handle = Handle(ui.view_loaded, None) #type: ignore
+			def loaded_view(v: sublime.View) -> None:
+				if view.id() == v.id():
+					future_view.set_result(view)
+					handle.dispose()
+			handle.callback = loaded_view
+			ui.view_loaded.add_handle(handle)
+		else:
+			future_view.set_result(view)
 
+	sublime.set_timeout(on_sublime_main, 0)
+
+	view = yield from future_view
 	assert not view.is_loading(), "?? why is this view still loading?"
+
+	if line is None:
+		return view
 	view.show(view.text_point(line, 0), True)
 	return view
 	
