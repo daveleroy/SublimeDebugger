ffd410a0119bd2cf392845055a82d2c9873b114a
David <2889367+daveleroy@users.noreply.github.com>
2019-04-13 23:13:02 -0700

    Fixes issue where breakpoints would get added to empty documents when you selected a line.

--- ui/events.py
+++ ui/events.py
@@ -44,7 +44,6 @@ class ViewEventsListener(sublime_plugin.EventListener):
 	# if a click is detected we then reselect the previous selection
 	# This means that a click in the gutter no longer selects that line
 	def on_text_command(self, view: sublime.View, cmd: str, args: dict) -> None:
-		print(cmd, args)
 		if cmd == 'drag_select' and 'event' in args:
 			view_drag_select.post(view)
 			event = args['event']
@@ -61,12 +60,10 @@ class ViewEventsListener(sublime_plugin.EventListener):
 				return
 			self.line = view.rowcol(pt)[0]
 			self.was_drag_select = True
-			print('was drag select')
 
 	def on_hover(self, view: sublime.View, point: int, hover_zone: int) -> None:
 		if hover_zone == sublime.HOVER_GUTTER:
 			line = view.rowcol(point)[0]
-			print('on_gutter_hover: {}'.format(line))
 			view_gutter_hovered.post(GutterEvent(view, line))
 		elif hover_zone == sublime.HOVER_TEXT:
 			view_text_hovered.post(HoverEvent(view, point))
@@ -80,9 +77,14 @@ class ViewEventsListener(sublime_plugin.EventListener):
 			# a click on the gutter selects the full line
 			# where as a click on an empty line puts the caret on that line
 			s = view.sel()[0]
-			l = view.full_line(view.text_point(self.line, 0))
-			if s.a == 0 and s.b == view.size():
-				print('on_gutter_double_clicked: {}'.format(self.line))
+			size = view.size()
+
+			# since we are checking if the whole document has been selected
+			# but a single click selects a line
+			# then a single click in a document with 1 line will cause a false double click
+			if view.rowcol(size)[0] <= 1:
+				return
+			if s.a == 0 and s.b == size:
 				view_gutter_double_clicked.post(GutterEvent(view, self.line))
 				view.sel().clear()
 				view.sel().add(view.line(view.text_point(self.line, 0)))
