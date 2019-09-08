dc032ec714a9e0813575646719bd9903d755078c
David <2889367+daveleroy@users.noreply.github.com>
2019-09-08 16:42:46 -0700

    Removed old console panel

---
+++ modules/debugger/diff.py
@@ -0,0 +1,27 @@
+
+
+from ..typecheck import *
+if TYPE_CHECKING:
+	from .client import DebugAdapterClient
+
+
+T = TypeVar('T')
+
+class DiffCollection (Generic[T]):
+	def __init__(self, on_added:Callable[[T], None], on_removed: Callable[[T], None]):
+		self.items = []
+		self.on_added = on_added
+		self.on_removed = on_removed
+
+	def update(self, new_items: List[T]):
+		for item in self.items:
+			if not item in new_items:
+				self.on_removed(item)
+				
+
+		for new_item in new_items:
+			if not new_item in self.items:
+				self.on_added(new_item)
+
+		self.items = list(new_items)
+				
