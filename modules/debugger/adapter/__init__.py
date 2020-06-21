d5f65592da3ce91115ddef76863b962878b4f0ec
Ruhul Alam <me@ruhul.io>
2020-06-21 14:33:06 -0500

    PR comments
    - Follow vscode logic for attach
    - Remove experimental settings and use debugpy by default

--- modules/debugger/adapter/__init__.py
+++ modules/debugger/adapter/__init__.py
@@ -2,5 +2,5 @@ from .adapter import Adapter, Adapters
 from .configuration import Configuration, ConfigurationExpanded, ConfigurationCompound
 from .transports import CommandSocketTransport, SocketTransport, StdioTransport
 from . import vscode
-from .dependencies import get_and_warn_require_node, get_and_warn_require_node_less_than_or_equal
+from .dependencies import get_and_warn_require_node
 from ..adapters import *
