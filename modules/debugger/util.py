b821795d8e8587d506bf194d27106ab0125064fc
David <2889367+daveleroy@users.noreply.github.com>
2020-11-08 00:15:47 -0800

    Adds utility to select a process

--- modules/debugger/util.py
+++ modules/debugger/util.py
@@ -13,3 +13,16 @@ async def select_process():
 	list = []
 	selected_proc: Any = None
 
+	def select(proc: Any):
+		nonlocal selected_proc
+		selected_proc = proc
+
+	for proc in psutil.process_iter(['pid', 'name', 'username']):
+		list.append(ui.InputListItem(lambda proc=proc: select(proc), proc.name() + f'\t{proc.pid}'+ f'\t{proc.username()}'))
+
+	await ui.InputList(list, "Select a process to start debugging").run()
+
+	if selected_proc:
+		return selected_proc.pid
+
+	raise core.Error("No process selected")
