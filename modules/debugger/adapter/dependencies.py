5a03cc28e03a3ce125e87c1a45722ce458bb8783
David <2889367+daveleroy@users.noreply.github.com>
2020-11-07 23:56:49 -0800

    Warn about node v13 possibly not being supported for adapters that require node

--- modules/debugger/adapter/dependencies.py
+++ modules/debugger/adapter/dependencies.py
@@ -21,8 +21,14 @@ def get_node_path(adapter_type: str):
 
 def get_and_warn_require_node(adapter_type: str, log: core.Logger):
 	node_path = get_node_path(adapter_type)
+	max_version = 'v13.0.0'
+
 	try:
-		subprocess.check_output([node_path, '-v'])
+		version = subprocess.check_output([node_path, '-v']).strip().decode('utf-8')
+
+		if version and version_tuple(version) >= version_tuple(max_version):
+			log.error(f'This adapter may not run on your version of node. It may require a version less than {max_version}. The version of node found is {version}.')
+
 	except Exception as e:
 		log.error(f'This adapter requires node it looks like you may not have node installed or it is not on your path: {e}. \nhttps://nodejs.org/')
 
