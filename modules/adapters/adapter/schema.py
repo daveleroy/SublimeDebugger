01474e69c9fc113b61fcfb298ef473bd5487717f
David <2889367+daveleroy@users.noreply.github.com>
2021-08-28 00:28:32 -0700

    Improved json schema generation so it now warns about unrecognized properties

--- modules/adapters/adapter/schema.py
+++ modules/adapters/adapter/schema.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ...typecheck import*
 from ...import dap
 from ...import core
@@ -6,45 +7,85 @@ import sublime
 import json
 import os
 
+
 def save_schema(adapters: list[dap.AdapterConfiguration]):
 
-	allOf = []
+	allOf: list[Any] = []
+	installed_adapters: list[str] = []
 	for adapter in adapters:
-		if not adapter.configuration_schema:
-			continue
+		if adapter.installed_version:
+			installed_adapters.append(adapter.type)
 
-		for key, value in adapter.configuration_schema.items():
+	definitions = {}
+
+	for adapter in adapters:
+		schema = adapter.configuration_schema or {}
+
+		requests: list[str] = []
+		for key, value in schema.items():
+			requests.append(key)
+
+		definitions[adapter.type] = {
+			'properties': {
+				'request': {
+					'type': 'string',
+					'description': F'Request type of configuration.',
+					'enum': requests,
+				},
+				'name': {
+					'type': 'string',
+					'description': 'Name of configuration; appears in the launch configuration drop down menu.',
+				},
+				'pre_debug_task': {
+					'type': 'string',
+					'description': 'Name of task to run before debugging starts',
+				},
+				'post_debug_task': {
+					'type': 'string',
+					'description': 'name of task to run after debugging ends',
+				}
+			}
+		}
+
+		allOf.append({
+			'if': {
+				'properties': {'type': { 'const': adapter.type }},
+			},
+			'then': {
+				'$ref': F'#/definitions/{adapter.type}'
+			},
+		})
+
+		for key, value in schema.items():
+			# make sure all the default properties are defined here because we are setting additionalProperties to false
+			value['additionalProperties'] = False
+			value.setdefault('properties', {})
+			value.setdefault('type', 'object')
+			value['properties']['name'] = { 'type':'string' }
+			value['properties']['type'] = { 'type':'string' }
+			value['properties']['request'] = { 'type':'string' }
+			value['properties']['pre_debug_task'] = { 'type':'string' }
+			value['properties']['post_debug_task'] = { 'type':'string' }
+			
+			definitions[f'{adapter.type}_{key}'] = value
+			
 			allOf.append({
 				'if': {
 					'properties': {'type': { 'const': adapter.type }, 'request': { 'const': key }},
 				},
-				'then': value,
+				'then': {
+					'$ref': F'#/definitions/{adapter.type}_{key}'
+				},
 			})
 
 	debugger_configurations = {
 		'type': 'object',
 		'properties': {
-			'name': {
-				'description': 'Name of configuration; appears in the launch configuration drop down menu.',
-				'type':'string'
-			},
 			'type': {
 				'type':'string',
-				'description': 'Type of configuration.'
+				'description': 'Type of configuration.',
+				'enum': installed_adapters,
 			},
-			'request': {
-				'type': 'string',
-				'description': 'Request type of configuration. Can be "launch" or "attach".',
-				'enum': [ 'attach', 'launch' ]
-			},
-			'pre_debug_task': {
-				'type': 'string',
-				'description': 'Name of task to run before debugging starts',
-			},
-			'post_debug_task': {
-				'type': 'string',
-				'description': 'name of task to run after debugging ends',
-			}
 		},
 		'required': ['type', 'name', 'request'],
 		'allOf': allOf,
@@ -86,6 +127,7 @@ def save_schema(adapters: list[dap.AdapterConfiguration]):
 			'settings':[{
 				'file_patterns': ['/*.sublime-project'],
 				'schema': {
+					'definitions': definitions,
 					'properties': {
 						'debugger_configurations': {
 							'description': 'Debugger Configurations',
@@ -110,4 +152,4 @@ def save_schema(adapters: list[dap.AdapterConfiguration]):
 
 	path = os.path.join(core.current_package(), 'sublime-package.json')
 	with open(path, 'w') as file:
-		file.write(sublime.encode_value(schema_debug_configurations, True))
+		file.write(json.dumps(schema_debug_configurations, indent='  '))
