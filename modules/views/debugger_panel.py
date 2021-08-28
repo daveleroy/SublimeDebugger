3ae8e494adfbc2329b0ec09ccabc5a000a622724
David <2889367+daveleroy@users.noreply.github.com>
2021-08-28 00:39:52 -0700

    Add tool tips to the debugger controls

--- modules/views/debugger_panel.py
+++ modules/views/debugger_panel.py
@@ -29,34 +29,34 @@ class DebuggerPanel(ui.div):
 		buttons = [] #type: list[ui.span]
 
 		items = [
-			DebuggerCommandButton(self.debugger.on_settings, ui.Images.shared.settings),
-			DebuggerCommandButton(self.debugger.on_play, ui.Images.shared.play),
+			DebuggerCommandButton(self.debugger.on_settings, ui.Images.shared.settings, 'Settings'),
+			DebuggerCommandButton(self.debugger.on_play, ui.Images.shared.play, 'Start'),
 		]
 
 
 		if self.debugger.is_stoppable():
-			items.append(DebuggerCommandButton(self.debugger.on_stop, ui.Images.shared.stop))
+			items.append(DebuggerCommandButton(self.debugger.on_stop, ui.Images.shared.stop, 'Stop'))
 		else:
-			items.append(DebuggerCommandButton(self.debugger.on_stop, ui.Images.shared.stop_disable))
+			items.append(DebuggerCommandButton(self.debugger.on_stop, ui.Images.shared.stop_disable, 'Stop (Disabled)'))
 
 		if self.debugger.is_running():
-			items.append(DebuggerCommandButton(self.debugger.on_pause, ui.Images.shared.pause))
+			items.append(DebuggerCommandButton(self.debugger.on_pause, ui.Images.shared.pause, 'Pause'))
 		elif self.debugger.is_paused():
-			items.append(DebuggerCommandButton(self.debugger.on_resume, ui.Images.shared.resume))
+			items.append(DebuggerCommandButton(self.debugger.on_resume, ui.Images.shared.resume, 'Continue'))
 		else:
-			items.append(DebuggerCommandButton(self.debugger.on_pause, ui.Images.shared.pause_disable))
+			items.append(DebuggerCommandButton(self.debugger.on_pause, ui.Images.shared.pause_disable, 'Pause (Disabled)'))
 
 		if self.debugger.is_paused():
 			items.extend([
-				DebuggerCommandButton(self.debugger.on_step_over, ui.Images.shared.down),
-				DebuggerCommandButton(self.debugger.on_step_out, ui.Images.shared.left),
-				DebuggerCommandButton(self.debugger.on_step_in, ui.Images.shared.right),
+				DebuggerCommandButton(self.debugger.on_step_over, ui.Images.shared.down, 'Step Over'),
+				DebuggerCommandButton(self.debugger.on_step_out, ui.Images.shared.left, 'Step Out'),
+				DebuggerCommandButton(self.debugger.on_step_in, ui.Images.shared.right, 'Step In'),
 			])
 		else:
 			items.extend([
-				DebuggerCommandButton(self.debugger.on_step_over, ui.Images.shared.down_disable),
-				DebuggerCommandButton(self.debugger.on_step_out, ui.Images.shared.left_disable),
-				DebuggerCommandButton(self.debugger.on_step_in, ui.Images.shared.right_disable),
+				DebuggerCommandButton(self.debugger.on_step_over, ui.Images.shared.down_disable, 'Step Over (Disabled)'),
+				DebuggerCommandButton(self.debugger.on_step_out, ui.Images.shared.left_disable, 'Step Out (Disabled)'),
+				DebuggerCommandButton(self.debugger.on_step_in, ui.Images.shared.right_disable, 'Step In (Disabled)'),
 			])
 
 		# looks like
@@ -97,16 +97,17 @@ class DebuggerPanel(ui.div):
 
 
 class DebuggerCommandButton (ui.span):
-	def __init__(self, callback: Callable[[], None], image: ui.Image) -> None:
+	def __init__(self, callback: Callable[[], None], image: ui.Image, title: str) -> None:
 		super().__init__()
 
 		self.image = image
 		self.callback = callback
+		self.title = title
 
 	def render(self) -> ui.span.Children:
 		return [
 			ui.span(css=css.padding)[
-				ui.click(self.callback)[
+				ui.click(self.callback, title=self.title)[
 					ui.icon(self.image),
 				]
 			]
