89c481fbde5780dffbe7249dab4f3384ecd57b71
David <2889367+daveleroy@users.noreply.github.com>
2021-12-19 14:03:15 -0800

    Updated dart adapter to use `dart debug-adapter`

--- modules/adapters/dart.py
+++ modules/adapters/dart.py
@@ -4,84 +4,34 @@ from ..import dap
 from .import adapter
 
 import shutil
-import re
-import subprocess
 
-# this is what the example configuration in examples/dart/dart.sublime-project sent to the dart adapter when using vscode
-
-# {
-#     "name": "Dart",
-#     "type": "dart",
-#     "request": "launch",
-#     "program": ".../Packages/Debugger/examples/dart/main.dart",
-#     "__configurationTarget": 5,
-#     "cwd": ".../Packages/Debugger/examples",
-#     "dartVersion": "2.10.3",
-#     "toolEnv": {
-#         "FLUTTER_HOST": "VSCode",
-#         "PUB_ENVIRONMENT": "vscode.dart-code"
-#     },
-#     "sendLogsToClient": true,
-#     "args": [],
-#     "vmAdditionalArgs": [],
-#     "vmServicePort": 0,
-#     "dartPath": "../dart",
-#     "maxLogLineLength": 2000,
-#     "pubPath": "../pub",
-#     "pubSnapshotPath": "../snapshots/pub.dart.snapshot",
-#     "debugSdkLibraries": false,
-#     "debugExternalLibraries": false,
-#     "showDartDeveloperLogs": true,
-#     "evaluateGettersInDebugViews": true,
-#     "evaluateToStringInDebugViews": true,
-#     "debuggerType": 0
-# }
+# See https://github.com/dart-lang/sdk/blob/main/pkg/dds/tool/dap/README.md
 
 class Dart(adapter.AdapterConfiguration):
-	type = "dart"
+	type = 'dart'
 
 	async def start(self, log: core.Logger, configuration: dap.ConfigurationExpanded):
-		node = await adapter.get_and_warn_require_node(self.type, log)
-		install_path = adapter.vscode.install_path(self.type)
+		if not shutil.which('dart'):
+			raise core.Error('You must install `dart` see https://dart.dev/get-dart')
+
 		command = [
-			node,
-			f'{install_path}/extension/out/dist/debug.js',
-			'dart'
+			'dart',
+			'debug_adapter',
 		]
 		return adapter.StdioTransport(log, command)
 
 	async def install(self, log: core.Logger):
-		url = await adapter.git.latest_release_vsix('Dart-Code', 'Dart-Code')
-		await adapter.vscode.install(self.type, url, log)
-
-	async def configuration_resolve(self, configuration: dap.ConfigurationExpanded):
-		# patch in dartPath and dartVersion
-		# that seems to be the minimum required options for the test example
-		# other configurations are likely to require more
-
-		dart = shutil.which('dart')
-		if not dart:
-			raise core.Error("Unable to find dart")
-
-		version = (await adapter.Process.check_output([dart, '--version'], stderr=subprocess.STDOUT)).decode('utf-8')
-		version_match = re.match(r'Dart SDK version: (\d+\.\d+\.\d+)', version)
-
-		if (not version_match):
-			raise core.Error("Unable to find dart version")
-
-		configuration['dartPath'] = dart
-		configuration['dartVersion'] = version_match[1]
-
-		return configuration
+		...
 
 	@property
 	def installed_version(self):
-		return adapter.vscode.installed_version(self.type)
+		return '???'
 
 	@property
 	def configuration_snippets(self):
-		return adapter.vscode.configuration_snippets(self.type)
+		return []
 
 	@property
 	def configuration_schema(self):
-		return adapter.vscode.configuration_schema(self.type)
+		return {}
+
