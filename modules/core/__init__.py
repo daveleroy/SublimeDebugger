d60b41aa3c2da225788d6bd8478d7484ce820979
David <2889367+daveleroy@users.noreply.github.com>
2022-01-30 08:27:37 -0800

    Fixes an issue where packages_path returns null during on_exit which is required for saving project data

--- modules/core/__init__.py
+++ modules/core/__init__.py
@@ -12,9 +12,10 @@ from .error import Error
 from .json import json_encode, json_decode
 
 _current_package = __package__.split('.', 1)[0]
+_current_package_path = os.path.join(sublime.packages_path(), _current_package)
 
 def current_package() -> str:
-	return os.path.join(sublime.packages_path(), _current_package)
+	return _current_package_path
 
 def current_package_name() -> str:
 	return _current_package
