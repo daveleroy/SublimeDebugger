298093da9ef46bcd285538e33faea0ef2efc1b1d
David <2889367+daveleroy@users.noreply.github.com>
2022-06-17 20:59:14 -0700

    Updated images

--- images/convert.sh
+++ images/convert.sh
@@ -10,7 +10,7 @@ do
 	file_blur_temp="magick_cache/${filename_no_ext}_blur.png"
 
 	# temp background blurred image
-	magick convert -alpha set -background none -channel A -evaluate multiply 0.5 +channel -resize 32x32 -blur 10x10 "base/${filename}" "${file_blur_temp}"
+	magick convert -alpha set -background none -channel A -evaluate multiply 0.1 +channel -resize 32x32 -blur 10x10 "base/${filename}" "${file_blur_temp}"
 	# temp negated image
 	magick convert -resize 32x32 -channel RGB -negate "base/${filename}" "${file_negated_temp}"
 
