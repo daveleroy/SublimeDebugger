a933215da8101d7496e68868a692845673c3983d
David <2889367+daveleroy@users.noreply.github.com>
2023-03-12 23:27:00 -0700

    Use our own expand_variables that doesn't just silently ignore unresolved variables and replace them with nothing

--- modules/dap/configuration.py
+++ modules/dap/configuration.py
@@ -178,6 +178,8 @@ class TaskExpanded(Task):
 		# if we don't remove these additional arguments Default.exec.ExecCommand will be unhappy
 		super().__init__(arguments, task.source)
 
+
+
 		cmd: str|list[str]|None = arguments.get('cmd')
 
 		if 'name' in arguments:
@@ -189,6 +191,7 @@ class TaskExpanded(Task):
 		else:
 			name = 'Untitled'
 
+		self.variables = variables
 		self.name: str = name
 		self.background: bool = arguments.get('background', False)
 		self.start_file_regex: str|None = arguments.get('start_file_regex')
@@ -199,7 +202,32 @@ class TaskExpanded(Task):
 				del self[key]
 
 
-def _expand_variables_and_platform(json: dict[str, Any], variables: dict[str, str] | None):
+def _expand_variables(json: Any, variables: dict[str, str], supress_errors: bool = False):
+	if type(json) is str:
+		regex = re.compile('(\${(.*)})')
+		for match, key in regex.findall(json):
+			if value := variables.get(key):
+				json = json.replace(match, value)
+			elif not supress_errors:
+				error = f'Unable to expand variable ${{{key}}}, available variables\n\n'
+				for variable in variables:
+					error += f'${{{variable}}}\n'
+
+				raise core.Error(error)
+
+		return json
+
+	if type(json) is dict:
+		for key in json.keys():
+			json[key] = _expand_variables(json[key], variables, supress_errors)
+
+	if type(json) is list:
+		for i in range(0, len(json)):
+			json[i] = _expand_variables(json[i], variables, supress_errors)
+
+	return json
+
+def _expand_variables_and_platform(json: dict[str, Any], variables: dict[str, str]):
 	json = json.copy()
 
 	platform = None
@@ -217,10 +245,10 @@ def _expand_variables_and_platform(json: dict[str, Any], variables: dict[str, st
 	# This allows us to add a list of variables to each configuration which case be use throughout the configuration.
 	# Its mostly so we can redefine $project_path when specifying a project file in debugger_configurations so $project_path refers to the correct location
 	if json_variables := json.get('$'):
-		json = sublime.expand_variables(json, json_variables)
+		json = _expand_variables(json, json_variables, supress_errors=True)
 		del json['$']
 
 	if variables := variables:
-		json = sublime.expand_variables(json, variables)
+		json = _expand_variables(json, variables)
 
 	return json
