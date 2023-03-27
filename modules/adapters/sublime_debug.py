5638c2d76e8f209229e56a9383c921dfd28868ed
David <2889367+daveleroy@users.noreply.github.com>
2023-03-26 22:17:06 -0700

    Changed how sublime debugging works so that plugin_host uses `connect` and the adapter users `listen`

--- modules/adapters/sublime_debug.py
+++ modules/adapters/sublime_debug.py
@@ -1,18 +1,16 @@
 from __future__ import annotations
-from dataclasses import dataclass
-import socket
 from typing import Any, TextIO
-import threading
-
-from .util import request
 
 from .import util
 from .. import dap
 from .. import core
 
+from dataclasses import dataclass
+
 import sublime
 import os
-
+import shutil
+import socket
 
 initial_preferences = '''{
 	"theme": "Debugger.sublime-theme",
@@ -52,16 +50,19 @@ class SublimeInstaller(dap.AdapterInstaller):
 		arch = sublime.arch()
 
 		path = self.temporary_install_path()
+		path_app = f'{path}/{self.debug_app_name}'
 
 		if platform == 'osx':
-			await request.download_and_extract_zip(f'https://download.sublimetext.com/sublime_text_build_{sublime.version()}_mac.zip', f'{path}/{self.debug_app_name}', log)
+			await util.request.download_and_extract_zip(f'https://download.sublimetext.com/sublime_text_build_{sublime.version()}_mac.zip', path_app, log)
 		elif platform == 'windows' and arch =='x64':
-			await request.download_and_extract_zip(f'https://download.sublimetext.com/sublime_text_build_{sublime.version()}_x64.zip', f'{path}/{self.debug_app_name}', log)
+			await util.request.download_and_extract_zip(f'https://download.sublimetext.com/sublime_text_build_{sublime.version()}_x64.zip', path_app, log)
 		else:
 			raise core.Error('Install for this platform/arch is not currently supported')
 
-		await request.download_and_extract_zip('https://github.com/microsoft/debugpy/archive/refs/tags/v1.6.6.zip', f'{path}/debugpy', log)
-		await request.download_and_extract_zip('https://github.com/daveleroy/debugpy/archive/refs/tags/v1.5.1.zip', f'{path}/debugpy_for_3.3', log)
+		core.remove_file_or_dir(f'{path_app}/Data')
+
+		await util.request.download_and_extract_zip('https://github.com/microsoft/debugpy/archive/refs/tags/v1.6.6.zip', f'{path}/debugpy', log)
+		await util.request.download_and_extract_zip('https://github.com/daveleroy/debugpy/archive/refs/tags/v1.5.1.zip', f'{path}/debugpy_for_3.3', log)
 
 	async def installable_versions(self, log: core.Logger) -> list[str]:
 		return [sublime.version()]
@@ -74,10 +75,21 @@ class Sublime(dap.AdapterConfiguration):
 	installer = SublimeInstaller()
 
 	async def start(self, log: core.Logger, configuration: dap.ConfigurationExpanded):
+		python = configuration.get('pythonPath') or configuration.get('python') or shutil.which('python3') or shutil.which('python')
+		if not python:
+			raise core.Error('Unable to find `python3` or `python`')
+
+		log.info('Using python `{}`'.format(python))
+
 		if configuration.request == 'attach':
-			return await dap.SocketTransport.connect_with_retry(log, 'localhost', configuration['port'])
+			command = [
+				f'{python}',
+				f'{self.installer.install_path()}/debugpy/src/debugpy/adapter',
+			]
+			return dap.StdioTransport(log, command)
 
 		return SublimeDebugTransport(configuration, log)
+
 	async def on_custom_event(self, session: dap.Session, event: str, body: Any):
 		if event == 'attach':
 			configuration = dap.Configuration.from_json(body, -1)
@@ -148,10 +160,20 @@ class SublimeDebugTransport(dap.TransportProtocol, dap.Transport):
 	def __init__(self, configuration: dap.ConfigurationExpanded, log: core.Logger) -> None:
 		super().__init__(self)
 
+		# grab open ports for debugpy
+		self.port_33 = util.get_open_port()
+		self.port_38 = util.get_open_port()
+
 		install_path = Sublime.installer.install_path()
 		data_directory = f'{install_path}/Data'
 		packages_directory = f'{install_path}/Data/Packages'
 		origin_packages_directory = sublime.packages_path()
+
+
+		python = configuration.get('pythonPath') or configuration.get('python') or shutil.which('python3') or shutil.which('python')
+		if not python:
+			raise core.Error('Unable to find `python3` or `python`')
+
 
 		if sublime.platform() == 'osx':
 			sublime_text_directory = f'{install_path}/{Sublime.installer.debug_app_name}/Contents/MacOS'
@@ -183,9 +205,9 @@ class SublimeDebugTransport(dap.TransportProtocol, dap.Transport):
 
 
 		# Add plugin that initializes the sublime_debug_runtime module before any other packages so it can attach debugpy before any other user packages get loaded
-		core.make_directory(f'{data_directory}/Packages/0_sublime_debug_runtime')
-		core.write(f'{packages_directory}/0_sublime_debug_runtime/__init__.py', 'import sublime_debug_runtime', overwrite_existing=True)
-		core.write(f'{packages_directory}/0_sublime_debug_runtime/.python-version', '3.8', overwrite_existing=True)
+		core.make_directory(f'{data_directory}/Packages/0_sublime_debug_runtime_38')
+		core.write(f'{packages_directory}/0_sublime_debug_runtime_38/__init__.py', 'import sublime_debug_runtime', overwrite_existing=True)
+		core.write(f'{packages_directory}/0_sublime_debug_runtime_38/.python-version', '3.8', overwrite_existing=True)
 
 		core.make_directory(f'{data_directory}/Packages/0_sublime_debug_runtime_33')
 		core.write(f'{packages_directory}/0_sublime_debug_runtime_33/__init__.py', 'import sublime_debug_runtime', overwrite_existing=True)
@@ -225,84 +247,55 @@ class SublimeDebugTransport(dap.TransportProtocol, dap.Transport):
 		settings = sublime.load_settings('Preferences.sublime-settings')
 		core.write(f'{packages_directory}/User/Debugger.sublime-theme', theme.format(settings.get("theme")), overwrite_existing=True)
 
-		# grab open ports for debugpy
-		port = util.get_open_port()
-
 		commands = [f'{sublime_text_directory}/sublime_text', '--multiinstance']
 		commands.extend(configuration.get('args', []))
 
 		self.log = log
-		self.port = port
 		self.path_mappings = path_mappings
 
-		self.clients: list[Client] = []
-
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.bind(('localhost', self.port))
-		self.socket.listen()
-
-		self.connections = [
-			self.accept_connections()
-		]
+		env = {
+			'sublime_debug_port_33': f'{self.port_33}',
+			'sublime_debug_port_38': f'{self.port_38}',
+			'sublime_debug_python': python,
+		}
+		env.update(os.environ)
 
 		log.info('Starting `Sublime Text (Debug)`')
-		self.process = dap.Process(commands,
-		env={
-			'sublime_debug_runtime_port': f'{port}',
-		})
-
+		self.process = dap.Process(commands, env=env)
 
 	def start(self, listener: dap.TransportProtocolListener, log: core.Logger):
 		self.events = listener
 		self.log = log
 
-	def attach(self, name: str, host: str, port: int):
 		self.events.on_event('attach', {
 			'type': 'sublime',
 			'request': 'attach',
-			'name': name,
-			'host': host,
-			'port': port,
+			'name': 'plugin_host_38',
+			'listen': {
+				'host': 'localhost',
+				'port': self.port_38,
+			},
 			'justMyCode': False,
 			'redirectOutput': True,
 			'pathMappings': self.path_mappings,
 		})
 
-	@core.run
-	async def accept_connections(self):
-		async def read(connection: socket.socket, port: int):
-			future = core.Future()
-			self.connections.append(future)
 
-			self.log.warn(f'sublime_debug_runtime: {port} connected ')
+		self.events.on_event('attach', {
+			'type': 'sublime',
+			'request': 'attach',
+			'name': 'plugin_host_33',
+			'listen': {
+				'host': 'localhost',
+				'port': self.port_33,
+			},
+			'justMyCode': False,
+			'redirectOutput': True,
+			'pathMappings': self.path_mappings,
+		})
 
-			file = connection.makefile('wr')
-			client = Client(file, connection)
-			self.clients.append(client)
 
-			while line := await core.run_in_executor(lambda: file.readline())():
 
-				data = core.json_decode(line)
-
-				event = data.get('event')
-				body = data.get('body', {})
-
-				if event == 'attach':
-					self.log.info(str(data))
-					self.attach(body['name'], body['host'], body['port'])
-				else:
-					self.log.info(str(data))
-
-			self.log.warn(f'sublime_debug_runtime: {port} disconnected')
-			future.set_result(None)
-
-		self.log.warn('Waiting on sublime_debug_runtime clients...')
-		while True:
-			try:
-				connection, addr = await core.run_in_executor(lambda: self.socket.accept())()
-				core.run(read(connection, addr[1]))
-			except:
-				break
 
 	async def initialized(self):
 		self.events.on_event('initialized', core.JSON({}))
@@ -324,10 +317,6 @@ class SublimeDebugTransport(dap.TransportProtocol, dap.Transport):
 
 		if command == 'disconnect':
 			self.dispose()
-
-			for connection in self.connections:
-				await connection
-
 			return {}
 
 		return {}
@@ -345,7 +334,3 @@ class SublimeDebugTransport(dap.TransportProtocol, dap.Transport):
 
 	def dispose(self):
 		self.process.dispose()
-		self.socket.close()
-
-		for client in self.clients:
-			client.close()
--- modules/adapters/sublime_debug_runtime.py
+++ modules/adapters/sublime_debug_runtime.py
@@ -11,42 +11,26 @@ import inspect
 import debugpy
 
 
-sublime_debug_runtime_port = int(os.environ['sublime_debug_runtime_port'])
 
-host_name = 'plugin_host_38' if sys.version_info >= (3, 8) else 'plugin_host_33'
+if sys.version_info >= (3, 8):
+	host_name = 'plugin_host_38'
+	port = int(os.environ['sublime_debug_port_38'])
+else:
+	host_name = 'plugin_host_33'
+	port = int(os.environ['sublime_debug_port_33'])
 
 
 class SublimeDebugRuntimeClient:
 	def __init__(self, name, port) -> None:
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.connect(('localhost', port))
-		self.socket_file = self.socket.makefile('wr')
-
-		debugpy.configure(python='python3')
-		address = debugpy.listen(0)
+		debugpy.configure()
+		debugpy.connect(port)
 		sublime.set_timeout_async(lambda: self.attach_to_thread('AsyncThread'))
-
-		self.send({
-			'event': 'attach',
-			'body': {
-				'name': name,
-				'host': address[0],
-				'port': address[1],
-			}
-		})
-
 		debugpy.wait_for_client()
+
 
 
 	def attach_to_thread(self, name: str):
 		threading.current_thread().setName(name)
 		debugpy.trace_this_thread(True)
 
-
-	def send(self, json):
-		self.socket_file.write(sublime.encode_value(json))
-		self.socket_file.write('\n')
-		self.socket_file.flush()
-
-client = SublimeDebugRuntimeClient(host_name, sublime_debug_runtime_port)
-
+client = SublimeDebugRuntimeClient(host_name, port)
