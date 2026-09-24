79c29b1afff0426e575274cdb3200387ec36d5c5
David <2889367+daveleroy@users.noreply.github.com>
2026-09-24 04:47:31 -0700

    elixir: check new and old paths and issue nicer error if adapter not found

--- modules/adapters/elixir.py
+++ modules/adapters/elixir.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import os
 from .. import dap
 from .. import core
 from . import util
@@ -14,5 +15,12 @@ class Elixir(dap.Adapter):
 	async def start(self, console: dap.Console, configuration: dap.ConfigurationExpanded):
 		install_path = self.installer.install_path()
 		extension = 'bat' if core.platform.windows else 'sh'
-		command = [f'{install_path}/elixir-ls-release/debug_adapter.{extension}']
-		return dap.StdioTransport(command, stderr=console.error)
+		paths = [
+			f'{install_path}/elixir-ls-release/debug_adapter.{extension}',
+			f'{install_path}/elixir-ls-release/debugger.{extension}',
+		]
+		for path in paths:
+			if os.path.exists(path):
+				return dap.StdioTransport([path], stderr=console.error)
+
+		raise dap.Error(f'Unable to find elixir debugger searched {paths}')
